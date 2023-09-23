31a3f1cb7a680131bab2632d4ff92438989f187c
solidity12 <volibear1001@gmail.com>
2023-09-24 03:56:54 +0900

    feat: Add deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+const hre = require("hardhat");
+
+const pairAddress = require("../config/contractAddrs.json")["WETH-Caerus-V2"];
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const UniswapV2TWAPOracle = await hre.ethers.getContractFactory("UniswapV2TWAPOracle");
+  const uniswapV2TWAPOracle = await UniswapV2TWAPOracle.deploy(pairAddress);
+
+  await uniswapV2TWAPOracle.deployed();
+
+  console.log("UniswapV2TWAPOracle deployed to:", uniswapV2TWAPOracle.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
